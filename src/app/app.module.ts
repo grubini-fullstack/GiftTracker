a403db79c9fb8b27724ad1a52d635353141044ce
giovanirubini <giovanirubinimaccagno@gmail.com>
2020-12-22 22:18:01 -0500

    added init routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,36 +3,55 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NavComponent } from './dashboard/nav/nav.component';
-import { SidemenuComponent } from './dashboard/sidemenu/sidemenu.component';
 import { MaincontentComponent } from './dashboard/maincontent/maincontent.component';
-import { MenuitemComponent } from './dashboard/sidemenu/menuitem/menuitem.component';
 import { ProductComponent } from './product/product.component';
 import { WatchlistComponent } from './watchlist/watchlist.component';
 import { WishlistComponent } from './wishlist/wishlist.component';
 import { BirthdaytrackerComponent } from './birthdaytracker/birthdaytracker.component';
 import { OrderComponent } from './order/order.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './account/profile/profile.component';
+import { NotificationComponent } from './account/notification/notification.component';
+import { HistoryComponent } from './account/history/history.component';
+import { PaymentComponent } from './account/payment/payment.component';
+
+const appRoutes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'watchlist', component: WatchlistComponent },
+  { path: 'wishlist', component: WishlistComponent },
+  { path: 'orders', component: OrderComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'notifications', component: NotificationComponent },
+  { path: 'history', component: HistoryComponent },
+  { path: 'payment', component: PaymentComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationComponent,
+    BirthdaytrackerComponent,
     DashboardComponent,
-    NavComponent,
-    SidemenuComponent,
+    HistoryComponent,
+    LoginComponent,
     MaincontentComponent,
-    MenuitemComponent,
+    NavComponent,
+    NotificationComponent,
     ProductComponent,
+    PaymentComponent,
+    ProfileComponent,
+    OrderComponent,
     WatchlistComponent,
     WishlistComponent,
-    BirthdaytrackerComponent,
-    OrderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
